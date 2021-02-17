51a629d1eae3e873e53ec8069bd2cb9d1f6a8041
Juha3141 <ianisnumber2027@gmail.com>
2021-02-17 10:31:49 +0900

    Made mouse demo program

--- Kernel64/Sources/Main.cpp
+++ Kernel64/Sources/Main.cpp
@@ -10,9 +10,25 @@
 
 extern "C" void Main(void) {
     System::Hardware::InitSystem();
+    int X = 100;
+    int Y = 100;
 
+    int DeltaX;
+    int DeltaY;
+    int Button;
+    VBEMODEINFOBLOCK *Block = (VBEMODEINFOBLOCK*)0x1000;
+    GraphicSystem System((unsigned short*)(Block->Address) , Block->Width , Block->Height);
 
+    System.DrawRectangle(X , Y , 9 , 9 , 0x00);
+    System.DrawRectangle(X+2 , Y+2 , 5 , 5 , RGB(0xFF , 0xFF , 0xFF));
     while(1) {
-        ;
+        if(System::Hardware::Mouse::GetMouseData(&(DeltaX) , &(DeltaY) , &(Button)) == false) {
+            continue;
+        }
+        X += DeltaX;
+        Y += DeltaY;
+
+        System.DrawRectangle(X , Y , 9 , 9 , 0x00);
+        System.DrawRectangle(X+2 , Y+2 , 5 , 5 , RGB(0xFF , 0xFF , 0xFF));
     }
 }
